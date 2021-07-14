d759ab9bf9675594d33647cc848efcc6c4197cb6
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-07-14 07:29:16 +0530

    Update strings.py

--- Sibyl_System/strings.py
+++ Sibyl_System/strings.py
@@ -56,5 +56,31 @@ bot_gban_string = """
 **Reason:** `{reason}`
 """
 
+ban_codes_string = """
+HERE ARE THE BAN CODES FOR EDITH -X :
+
+• `ED-X_01` - RAID PARTICIPANT
+• `ED-X_02` - RAID/SPAM INFLAMMER
+• `ED-X_03` - SCAMMER
+• `ED-X_04` - SPAM ADDING MEMBER
+• `ED-X_05` - ABUSE SPAM 
+• `ED-X_06` - NSFW SPAMMER
+• `ED-X_07` - IMPERSONATION
+• `ED-X_08` - MD/BTC SCAM
+• `ED-X_09` - ADDING SPAMBOTS
+• `ED-X_10` - ILLEGAL
+• `ED-X_11` - PHISHING
+• `ED-X_12` - FRAUD PROMOTION  (ANY KIND)
+• `ED-X_13` - CYBER THREATENING/CYBER BULLY 
+• `ED-X_14` - CHILD ABUSE 
+• `ED-X_15` - BAN EVASION 
+• `ED-X_16` - SPAMBOT
+• `ED-X_17` - RAID INITIALIZOR
+• `ED-X_18` - CRIMINAL ACT
+
+
+THIS SCANNER BANS ONLY APPLY TO YOUR GROUP IF U ARE USING ANY ONE OF THE BOTS IN [THIS LIST](https://t.me/EdithXinfo/12)
+"""
+
 # https://psychopass.fandom.com/wiki/Crime_Coefficient_(Index)
 # https://psychopass.fandom.com/wiki/The_Dominator
