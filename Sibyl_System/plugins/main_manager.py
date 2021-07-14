b28144b54ec425eb52c136121b56d3ddd50329c3
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-07-14 08:26:06 +0530

    Update main_manager.py

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -4,7 +4,6 @@ from Sibyl_System.strings import (
     reject_string,
     proof_string,
     forced_scan_string,
-    ban_codes_string,
 )
 from Sibyl_System import System, system_cmd
 from Sibyl_System.utils import seprate_flags, Flag
@@ -318,8 +317,31 @@ async def reject(event):
 
 @System.on(system_cmd(pattern=r"bancodes"))
 async def bancodes(event):
-      msg = await event.reply("Getting Info...")
-      await msg.edit("{ban_codes_string}")
+      msg = await event.reply("Getting Codes...")
+      time.sleep(2)
+      await msg.edit("HERE ARE THE BAN CODES FOR EDITH -X :
+
+• `ED-X_01` - RAID PARTICIPANT
+• `ED-X_02` - RAID/SPAM INFLAMMER
+• `ED-X_03` - SCAMMER
+• `ED-X_04` - SPAM ADDING MEMBER
+• `ED-X_05` - ABUSE SPAM 
+• `ED-X_06` - NSFW SPAMMER
+• `ED-X_07` - IMPERSONATION
+• `ED-X_08` - MD/BTC SCAM
+• `ED-X_09` - ADDING SPAMBOTS
+• `ED-X_10` - ILLEGAL
+• `ED-X_11` - PHISHING
+• `ED-X_12` - FRAUD PROMOTION  (ANY KIND)
+• `ED-X_13` - CYBER THREATENING/CYBER BULLY 
+• `ED-X_14` - CHILD ABUSE 
+• `ED-X_15` - BAN EVASION 
+• `ED-X_16` - SPAMBOT
+• `ED-X_17` - RAID INITIALIZOR
+• `ED-X_18` - CRIMINAL ACT
+
+
+THIS SCANNER BANS ONLY APPLY TO YOUR GROUP IF U ARE USING ANY ONE OF THE BOTS IN [THIS LIST](https://t.me/EdithXinfo/12)")
       return
 
 help_plus = """
